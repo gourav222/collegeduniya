acfb62863a8ae4a4813377cc77e417872e005acb
gourav222 <gouravparmar775566@gmail.com>
2023-12-25 11:05:58 +0530

    Add Utility

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,5 @@
 import { user } from "../models/userSchema.js";
-
+import sendToken from "../utility/sendToken.js";
 export const registerUser = async (req, res) => {
   try {
     if (
@@ -20,7 +20,7 @@ export const registerUser = async (req, res) => {
       return res.status(400).json({ message: "Phone number already exists" });
     }
     const users = await user.create(req.body);
-    res.status(200).json({ message: "Successfully registered", users });
+    sendToken(users, 201, res);
   } catch (error) {
     console.log(error);
   }
--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema({
   fullName: {
     type: String,
@@ -29,5 +30,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 export const user = mongoose.model("users", userSchema);
